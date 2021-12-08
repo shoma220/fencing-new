fa7fa2613899932da34b8f6560dd056c2a8d0dd6
shoma220 <87297746+shoma220@users.noreply.github.com>
2021-12-08 19:07:59 +0900

    Update Controller2.cs

--- Assets/Scenes/Controller2.cs
+++ Assets/Scenes/Controller2.cs
@@ -1,39 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class Controller2 : MonoBehaviour
 {
- 
-    // 移動時の加算用変数
     float vx = 0;
     float vy = 0;
- 
-    // 移動スピード
+
     public float speed = 30;
- 
+ 	public GameObject Rect;
+RectTransform rect;
     // Start is called before the first frame update
     void Start()
     {
- 
+ rect = Rect.GetComponent < RectTransform > ();
     }
  
     // Update is called once per frame
     void Update()
     {
         // 毎フレーム数値を初期化
-        vx = 0;
+        /*vx = 0;
         vy = 0;
- 
+ */
         // 横移動
-        if (Input.GetKey(KeyCode.A)) {
-            vx = -speed;
-        } else if (Input.GetKey(KeyCode.D)) {
-            vx = speed;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+ rect.localPosition += new Vector3(10, 0, 0);
+            //vx = -speed;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+ rect.localPosition -= new Vector3(10, 0, 0);
+            //vx = speed;
         }
  
-        // 実際の移動処理
-        this.transform.Translate(vx/50, vy/50, 0);
  
+        // 実際の移動処理
+        //this.transform.Translate();
+ //rect.localPosition += new Vector3(vx/50, vy/50, 0);
     }
 }
